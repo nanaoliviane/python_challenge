bdf1d692642195e3c84c3fec1cf794289d0a0ed1
nanaoliviane <nanaoliviane@gmaail.com>
2024-11-06 22:32:53 +0300

    made changes to my models for blog and registered blog as my installed apps in settings

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=180)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
