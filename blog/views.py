0ed9cc4e5c0b8911b9743d2e3c28325e4c95083c
nanaoliviane <nanaoliviane@gmaail.com>
2024-11-08 23:34:48 +0300

    added fetch posts command in my views file

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import BlogPost
+
+def post_list(request):
+    posts = BlogPost.objects.all().order_by('-created_at')  # Fetch all posts, ordering by newest first
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 # Create your views here.
