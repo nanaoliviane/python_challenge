a4476f6890620a975fbeef8814f9a8cc944572d1
nanaoliviane <nanaoliviane@gmaail.com>
2024-11-09 23:46:44 +0300

    added post_detal to make my blog more detailed mamking chnages in urls,models and views

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 from django.contrib.auth.models import User
 
 
@@ -9,6 +10,12 @@ class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)  # Add this line
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 def post_list(request):
     posts = BlogPost.objects.all().order_by('-created_at')  # Fetch all posts, ordering by newest first
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(BlogPost, slug=slug, status=1)
+    return render(request, 'blog/post_deatil.html', {'post': post})
+
 
 # Create your views here.
